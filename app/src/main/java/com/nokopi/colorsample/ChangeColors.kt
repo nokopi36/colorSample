c22842e62475642b3d4ecd910cc09711e1d831b6
BuildTools <unconfigured@null.spigotmc.org>
2022-05-27 00:50:47 +0900

    各パーツの色の順番を変更

--- app/src/main/java/com/nokopi/colorsample/ChangeColors.kt
+++ app/src/main/java/com/nokopi/colorsample/ChangeColors.kt
@@ -7,41 +7,43 @@ import android.widget.ImageView
 
 class ChangeColors {
     companion object{
-        val leathersColorSpinner = listOf(R.drawable.button_white, R.drawable.button_black, R.drawable.button_sepia, R.drawable.button_momosiotya, R.drawable.button_mediumslateblue, R.drawable.button_red,
-            R.drawable.button_blue, R.drawable.button_summergreen, R.drawable.button_mandarinorange, R.drawable.button_framingopink, R.drawable.button_buff)
-        val leathersColor = listOf(CustomColor.white, CustomColor.black, CustomColor.sepia, CustomColor.momosiotya, CustomColor.mediumslateblue, CustomColor.red,
-            CustomColor.blue, CustomColor.summergreen, CustomColor.mandarinorange, CustomColor.framingopink, CustomColor.buff)
+        val leathersColorSpinner = listOf(R.drawable.button_white, R.drawable.button_mandarinorange, R.drawable.button_red, R.drawable.button_buff, R.drawable.button_framingopink, R.drawable.button_mediumslateblue,
+            R.drawable.button_momosiotya, R.drawable.button_blue, R.drawable.button_summergreen, R.drawable.button_sepia, R.drawable.button_black)
+        val leathersColor = listOf(CustomColor.white, CustomColor.mandarinorange, CustomColor.red, CustomColor.buff, CustomColor.framingopink, CustomColor.mediumslateblue,
+            CustomColor.momosiotya, CustomColor.blue, CustomColor.summergreen, CustomColor.sepia, CustomColor.black)
 
-        val bandsColorSpinner = listOf(R.drawable.button_black, R.drawable.button_blue, R.drawable.button_lightblue, R.drawable.button_hotpink)
-        val bandsColor = listOf(CustomColor.black, CustomColor.blue, CustomColor.lightblue, CustomColor.hotpink)
+        val bandsColorSpinner = listOf(R.drawable.button_hotpink, R.drawable.button_blue, R.drawable.button_lightblue, R.drawable.button_black)
+        val bandsColor = listOf(CustomColor.hotpink, CustomColor.blue, CustomColor.lightblue, CustomColor.black)
 
-        val spongesColorSpinner = listOf(R.drawable.button_lightsalmon, R.drawable.button_white, R.drawable.button_black, R.drawable.button_mint, R.drawable.button_lightgreen, R.drawable.button_deepskyblue
-            , R.drawable.button_orange, R.drawable.button_pink)
-        val spongesColor = listOf(CustomColor.lightsalmon, CustomColor.white, CustomColor.black, CustomColor.mint, CustomColor.lightgreen, CustomColor.deepskyblue
-            , CustomColor.orange, CustomColor.pink)
+        val spongesColorSpinner = listOf(R.drawable.button_white, R.drawable.button_lightsalmon, R.drawable.button_black, R.drawable.button_orange, R.drawable.button_pink, R.drawable.button_deepskyblue
+            , R.drawable.button_mint, R.drawable.button_lightgreen)
+        val spongesColor = listOf(CustomColor.white, CustomColor.lightsalmon, CustomColor.black, CustomColor.orange, CustomColor.pink, CustomColor.deepskyblue
+            , CustomColor.mint, CustomColor.lightgreen)
 
-        val plasticsColorSpinner = listOf(R.drawable.button_white, R.drawable.button_black, R.drawable.button_blue, R.drawable.button_green, R.drawable.button_red)
-        val plasticsColor = listOf(CustomColor.white, CustomColor.black, CustomColor.blue, CustomColor.green, CustomColor.red)
+        val plasticsColorSpinner = listOf(R.drawable.button_white, R.drawable.button_red, R.drawable.button_blue, R.drawable.button_green, R.drawable.button_black)
+        val plasticsColor = listOf(CustomColor.white, CustomColor.red, CustomColor.blue, CustomColor.green, CustomColor.black)
 
-        val stringsColorSpinner = listOf(R.drawable.button_orange, R.drawable.button_black, R.drawable.button_sepia, R.drawable.button_lightblue, R.drawable.button_mediumslateblue, R.drawable.button_blue,
-            R.drawable.button_red, R.drawable.button_lightgreen, R.drawable.button_mandarinorange, R.drawable.button_framingopink, R.drawable.button_beigerose, R.drawable.button_sapphireblue)
-        val stringsColor = listOf(CustomColor.orange, CustomColor.black, CustomColor.sepia, CustomColor.lightblue, CustomColor.mediumslateblue, CustomColor.blue,
-            CustomColor.red, CustomColor.lightgreen, CustomColor.mandarinorange, CustomColor.framingopink, CustomColor.beigerose, CustomColor.sapphireblue)
+        val stringsColorSpinner = listOf(R.drawable.button_white, R.drawable.button_mandarinorange, R.drawable.button_orange, R.drawable.button_red, R.drawable.button_beigerose, R.drawable.button_framingopink,
+            R.drawable.button_mediumslateblue, R.drawable.button_sapphireblue, R.drawable.button_blue, R.drawable.button_lightblue, R.drawable.button_green, R.drawable.button_lightgreen, R.drawable.button_sepia,
+            R.drawable.button_black)
+        val stringsColor = listOf(CustomColor.white, CustomColor.mandarinorange, CustomColor.orange, CustomColor.red, CustomColor.beigerose, CustomColor.framingopink,
+            CustomColor.mediumslateblue, CustomColor.sapphireblue, CustomColor.blue, CustomColor.lightblue, CustomColor.green, CustomColor.lightgreen, CustomColor.sepia,
+            CustomColor.black)
 
-        val buttonsColorSpinner = listOf(R.drawable.button_white, R.drawable.button_black, R.drawable.button_blue, R.drawable.button_red, R.drawable.button_green, R.drawable.button_yamabukitya, R.drawable.button_sepia,
-            R.drawable.button_framingopink, R.drawable.button_vanilla)
-        val buttonsColor = listOf(CustomColor.white, CustomColor.black, CustomColor.blue, CustomColor.red, CustomColor.green, CustomColor.yamabukitya, CustomColor.sepia,
-            CustomColor.framingopink, CustomColor.vanilla)
+        val buttonsColorSpinner = listOf(R.drawable.button_white, R.drawable.button_yamabukitya, R.drawable.button_red, R.drawable.button_vanilla, R.drawable.button_framingopink, R.drawable.button_blue, R.drawable.button_green,
+            R.drawable.button_sepia, R.drawable.button_black)
+        val buttonsColor = listOf(CustomColor.white, CustomColor.yamabukitya, CustomColor.red, CustomColor.vanilla, CustomColor.framingopink, CustomColor.blue, CustomColor.green,
+            CustomColor.sepia, CustomColor.black)
 
-        val nbSLBPlasticsColorSpinner = listOf(R.drawable.button_lightsalmon, R.drawable.button_white, R.drawable.button_black, R.drawable.button_red, R.drawable.button_green, R.drawable.button_blue, R.drawable.button_deepskyblue, R.drawable.button_maroon,
-            R.drawable.button_hotpink)
-        val nbSLBPlasticsColor = listOf(CustomColor.lightsalmon, CustomColor.white, CustomColor.black, CustomColor.red,  CustomColor.green, CustomColor.blue, CustomColor.deepskyblue, CustomColor.maroon,
-            CustomColor.hotpink)
+        val nbSLBPlasticsColorSpinner = listOf(R.drawable.button_white, R.drawable.button_red, R.drawable.button_lightsalmon, R.drawable.button_hotpink, R.drawable.button_blue, R.drawable.button_deepskyblue, R.drawable.button_green,
+            R.drawable.button_maroon, R.drawable.button_black)
+        val nbSLBPlasticsColor = listOf(CustomColor.white, CustomColor.red, CustomColor.lightsalmon, CustomColor.hotpink, CustomColor.blue, CustomColor.deepskyblue, CustomColor.green,
+            CustomColor.maroon, CustomColor.black)
 
-        val plSpongesColorSpinner = listOf(R.drawable.button_white, R.drawable.button_black, R.drawable.button_mint, R.drawable.button_lightgreen, R.drawable.button_deepskyblue
-            , R.drawable.button_orange, R.drawable.button_pink)
-        val plSpongesColor = listOf(CustomColor.white, CustomColor.black, CustomColor.mint, CustomColor.lightgreen, CustomColor.deepskyblue
-            , CustomColor.orange, CustomColor.pink)
+        val plSpongesColorSpinner = listOf(R.drawable.button_white, R.drawable.button_orange, R.drawable.button_pink, R.drawable.button_deepskyblue, R.drawable.button_mint
+            , R.drawable.button_lightgreen, R.drawable.button_black)
+        val plSpongesColor = listOf(CustomColor.white, CustomColor.orange, CustomColor.pink, CustomColor.deepskyblue, CustomColor.mint
+            , CustomColor.lightgreen, CustomColor.black)
 
         val whiteBlackSpinner = listOf(R.drawable.button_white, R.drawable.button_black)
         val whiteBlack = listOf(CustomColor.white, CustomColor.black)
